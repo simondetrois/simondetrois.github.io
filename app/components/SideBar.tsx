2e43aaa3f9ef9a2391b73c9d6944b2bb8a09ef31
Simon Detrois <simondetrois@gmail.com>
2025-04-06 12:19:04 +0200

    feat: html converter automation
    * feat: added script setup
    
    * feat: added markdown script reader
    
    * feat: finished markdown to html converter

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import { useState, type Dispatch, type SetStateAction } from "react";
 import { Menu, Search, X } from "lucide-react";
+import * as fs from 'fs';
 
 interface SideBarProps  {
   sidebarOpen: boolean;
@@ -7,6 +8,13 @@ interface SideBarProps  {
 }
 
 const SideBar = ({sidebarOpen, setSidebarOpen}:SideBarProps) => {
+  const dir = fs.readdirSync("../generated", {withFileTypes: true, recursive: true})
+  const dirs = dir.filter(element => element.isDirectory())
+  const files = dir.filter(element => element.isFile())
+  const dirMap = new Map<string, fs.Dirent[]>()
+  dirs.forEach(dir => dirMap.set(dir.name, []))
+  files.forEach(file => dirMap.set(file.parentPath, [...dirMap.get(file.parentPath)!,file]))
+
   return (
     <aside
     className={`fixed inset-y-0 left-0 w-64 bg-gray-800 text-white p-4 transform transition-transform lg:translate-x-0 ${
